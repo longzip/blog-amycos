cc6893d1116d9f82da09a0ea8a3e4bbee6303206
Lo Van Long <info@lovanlong.ga>
2023-10-25 11:08:03 +0700

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,27 +1,18 @@
-import { CMS_NAME, CMS_URL } from "../lib/constants";
-
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Mỹ phẩm Amycos.
+        Blog Mẹo Làm Đẹp.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Ghé thăm{" "}
+        Là phụ nữ, nhất định phải xinh đẹp và tự tin. Hãy cùng{" "}
         <a
-          href="https://www.amycos.shop/"
+          href="https://www.amycos.vn/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          shop Mỹ Phẩm Amycos
+          Mỹ Phẩm Amycos
         </a>{" "}
-        và{" "}
-        <a
-          href="https://www.facebook.com/amycos.vn"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Cộng đồng Amycos
-        </a>
-        .
+        khám phá những bí quyết làm đẹp thú vị nhé .
       </h4>
     </section>
   );
--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,8 +1,8 @@
-import Avatar from './avatar'
-import Date from './date'
-import CoverImage from './cover-image'
-import PostTitle from './post-title'
-import Categories from './categories'
+import Avatar from "./avatar";
+import Date from "./date";
+import CoverImage from "./cover-image";
+import PostTitle from "./post-title";
+import Categories from "./categories";
 
 export default function PostHeader({
   title,
@@ -25,10 +25,10 @@ export default function PostHeader({
           <Avatar author={author} />
         </div>
         <div className="mb-6 text-lg">
-          Posted <Date dateString={date} />
+          Đăng <Date dateString={date} />
           <Categories categories={categories} />
         </div>
       </div>
     </>
-  )
+  );
 }
